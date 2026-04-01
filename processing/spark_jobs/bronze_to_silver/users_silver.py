39bd336f37f61c3a3fda2454ef018e1885504ebd
Brian2994 <brianpari29@gmail.com>
2026-03-31 23:27:36 -0300

    feat: implementa pipeline bronze_to_silver e estabiliza ambiente spark/postgres

--- processing/spark_jobs/bronze_to_silver/users_silver.py
+++ processing/spark_jobs/bronze_to_silver/users_silver.py
@@ -1,21 +1,70 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import  col, concat_ws, current_timestamp
+from pyspark.sql.functions import col, concat_ws, current_timestamp, coalesce, lit
+import os
 
-spark = SparkSession.builder \
-    .appName('UsersSilver') \
-    .master("local[*]") \
-    .getOrCreate()
+BRONZE_PATH = "data/bronze/users/"
+POSTGRES_URL = "jdbc:postgresql://postgres:5432/data_platform"
 
-df = spark.read.parquet('/opt/project/data/bronze/users/')
+def main():
+    print("🚀 Iniciando Spark...")
 
-df_clean = df.select(
-    col('id').alias('user_id'),
-    concat_ws(' ', col('name.firstname'), col('name.lastname')).alias('full_name'),
-    col('email'),
-    col('phone'),
-    col('address.city').alias('city'),
-    col('address.zipcode').alias('zipcode'),
-    current_timestamp().alias('processed_at')
-).dropDuplicates(['user_id'])
+    spark = SparkSession.builder \
+        .appName('UsersSilver') \
+        .master("local[*]") \
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3") \
+        .getOrCreate()
 
-df_clean.write.mode('overwrite').parquet('/opt/project/data/silver/users/')
+    # 🔹 Verifica se existe dado
+    if not os.path.exists(BRONZE_PATH) or len(os.listdir(BRONZE_PATH)) == 0:
+        print("❌ Nenhum arquivo encontrado na camada bronze")
+        return
+
+    print("📥 Lendo dados bronze...")
+
+    df = spark.read.parquet(BRONZE_PATH)
+
+    print(f"🔍 Registros lidos: {df.count()}")
+
+    # 🔹 Transformação (Silver)
+    df_clean = df.select(
+        col('id').alias('user_id'),
+        concat_ws(
+            ' ',
+            coalesce(col('`name.firstname`'), lit('')),
+            coalesce(col('`name.lastname`'), lit(''))
+        ).alias('full_name'),
+        col('email'),
+        col('phone'),
+        coalesce(col('`address.city`'), lit('unknown')).alias('city'),
+        coalesce(col('`address.zipcode`'), lit('unknown')).alias('zipcode'),
+        current_timestamp().alias('processed_at')
+    ).dropDuplicates(['user_id'])
+
+    print(f"✅ Registros após limpeza: {df_clean.count()}")
+
+    # 🔹 Escrita no Postgres
+    try:
+        print("💾 Salvando arquivo...")
+
+        df_clean.write \
+            .format("jdbc") \
+            .option("url", POSTGRES_URL) \
+            .option("driver", "org.postgresql.Driver") \
+            .option("dbtable", "users") \
+            .option("user", "postgres") \
+            .option("password", "postgres") \
+            .mode("overwrite") \
+            .save()
+
+        print("🎉 Users salvo com sucesso!")
+
+    except Exception as e:
+        print("❌ Erro ao salvar:")
+        print(e)
+
+    finally:
+        spark.stop()
+
+
+if __name__ == "__main__":
+    main()
