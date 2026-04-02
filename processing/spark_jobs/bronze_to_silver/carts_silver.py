d47116f8150ce1631e8bb2fc25a62eb7f56b8f98
Brian2994 <brianpari29@gmail.com>
2026-04-02 00:06:16 -0300

    feat(pipeline): implementa processamento bronze_to_silver para carts

--- processing/spark_jobs/bronze_to_silver/carts_silver.py
+++ processing/spark_jobs/bronze_to_silver/carts_silver.py
@@ -1,22 +1,65 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode, current_timestamp
+from pyspark.sql.functions import col, current_timestamp, coalesce, lit
+import os
 
-spark = SparkSession.builder \
-    .appName('CartsSlver') \
-    .getOrCreate()
+BRONZE_PATH = "data/bronze/carts/"
+POSTGRES_URL = "jdbc:postgresql://postgres:5432/data_platform"
 
-df = spark.read.parquet('data/bronze/carts/')
+def main():
+    print("🚀 Iniciando Spark...")
 
-# Explode produtos dentro do carrinho
-df_exploded = df.withColumn('product', explode('products'))
+    spark = SparkSession.builder \
+        .appName('CartsSilver') \
+        .master("local[*]") \
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3") \
+        .getOrCreate()
 
-df_clean = df_exploded.select(
-    col('id').alias('order_id'),
-    col('userId').alias('user_id'),
-    col('date').alias('order_date'),
-    col('product.productId').alias('product_id'),
-    col('product.quantity').alias('quantity'),
-    current_timestamp().alias('processed_at')
-)
+    # 🔹 Verifica se existe dado
+    if not os.path.exists(BRONZE_PATH) or len(os.listdir(BRONZE_PATH)) == 0:
+        print("❌ Nenhum arquivo encontrado na camada bronze")
+        return
 
-df_clean.write.mode('overwrite').parquet('data/silver/orders/')
+    print("📥 Lendo dados bronze...")
+
+    df = spark.read.parquet(BRONZE_PATH)
+
+    print(f"🔍 Registros lidos: {df.count()}")
+
+    # 🔹 Transformação (Silver)
+    df_clean = df.select(
+        col('cart_id').alias('order_id'),
+        col('user_id'),
+        col('product_id'),
+        coalesce(col('quantity'), lit(0)).alias('quantity'),
+        col('date').alias('order_date'),
+        current_timestamp().alias('processed_at')
+    )
+
+    print(f"✅ Registros após limpeza: {df_clean.count()}")
+
+    # 🔹 Escrita no Postgres
+    try:
+        print("💾 Salvando arquivo...")
+
+        df_clean.write \
+            .format("jdbc") \
+            .option("url", POSTGRES_URL) \
+            .option("driver", "org.postgresql.Driver") \
+            .option("dbtable", "carts") \
+            .option("user", "postgres") \
+            .option("password", "postgres") \
+            .mode("overwrite") \
+            .save()
+
+        print("🎉 Users salvo com sucesso!")
+
+    except Exception as e:
+        print("❌ Erro ao salvar:")
+        print(e)
+
+    finally:
+        spark.stop()
+
+
+if __name__ == "__main__":
+    main()
